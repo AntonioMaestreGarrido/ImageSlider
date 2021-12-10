e2db8d8ec3c4dc30f0a3eee0a1416d933824c0b5
Antonio <antoniomaestregarrido.com>
2021-12-10 03:45:53 +0100

    playing with timer

--- main.js
+++ main.js
@@ -1,40 +1,93 @@
 var test = document.querySelector("input");
-var imagen = document.querySelector("#imagen");
+
 var coleccion = [];
 
 console.log(test.files);
+const frame = document.querySelector("#frame")
+
 document.querySelector("#sub").addEventListener("click", selectFiles);
-document.querySelector("#test").addEventListener("click", agregaArchivo);
-var im;
+document.querySelector("#test").addEventListener("click", fillSlide);
+
+var timer = setInterval(changeBigPicture, 5000);
+timer()
+function changeBigPicture() {
+  let index = frame.querySelector("img").getAttribute("index");
+  console.log(index)
+  console.log(typeof(index))
+  index++
+  showBig(index)
+  console.log(typeof(index))
+}
 
 function selectFiles() {
-  coleccion=[]
+  coleccion = [];
   for (let i = 0; i < test.files.length; i++) {
     coleccion.push(test.files[i]);
   }
 }
-function agregaArchivo() {
-  var fr = new FileReader();
-  
-  fr.readAsDataURL(coleccion[1]);
 
-  fr.addEventListener("load", () => {
-    imagen.setAttribute("index", "w");
-    imagen.setAttribute("src", fr.result);
-    fillSlide()
-  });
-}
 
 function fillSlide() {
-  let slide=document.querySelector("#slide")
-  for (let i = 0; i < coleccion.length; i++) {
+  let slide = document.querySelector("#slide");
+  coleccion.forEach((im) => {
+    let fr = new FileReader();
+    fr.readAsDataURL(im);
+    fr.addEventListener("load", () => {
+      let index = coleccion.indexOf(im);
+      let newImage = document.createElement("img");
+      newImage.setAttribute("index", index);
+      newImage.setAttribute("src", fr.result);
+      newImage.classList.add("miniImg");
+      newImage.addEventListener("click", showBig);
+      slide.appendChild(newImage);
+    });
+  });
+}
+function showBig(e) {
+  let index
+  if(typeof(e)==="object"){
+     index = e.target.getAttribute("index");
+
+  }else if(typeof(e)==="number"){ index=e}
+  let imagen = document.querySelector("#imagen");
+  var fr = new FileReader();
+
+  fr.readAsDataURL(coleccion[index]);
+
+  fr.addEventListener("load", () => {
+    imagen.setAttribute("index", index);
+    imagen.setAttribute("src", fr.result);
+  });
+
+  ttest();
+}
+
+function ttest() {
+  let imagetest = document.querySelector("[index='2']");
+  imagetest.classList.remove("miniImg");
+
+
+  document.querySelector("#frame").click();
+  frame.click();
+}
+
+/*
+function fillSlide() {
+ 
+let slide=document.querySelector("#slide")
+for (let i = 0; i < coleccion.length; i++) {
+  let fr = new FileReader();
+  fr.readAsDataURL(coleccion[i]);
+  fr.addEventListener("load", () => {
+    
     let newImage=document.createElement("img")
     newImage.setAttribute("index",i)
-    newImage.textContent="uuh"
+    newImage.setAttribute("src", fr.result);
     newImage.classList.add("miniImg")
     slide.appendChild(newImage)
+    
+  });
 
-  }
-
-  
+ 
 }
+}*/
